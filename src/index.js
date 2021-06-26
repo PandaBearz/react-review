5a9863c78b4c14076014d306a18007b685973a15
Samuel VanCuren <samuel@vancuren.net>
2021-06-26 09:56:36 -0400

    Header and Footer

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'typeface-lobster';
 import 'typeface-open-sans';
 import './index.css';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 import App from './App';
-
 
 ReactDOM.render(
   <React.StrictMode>
